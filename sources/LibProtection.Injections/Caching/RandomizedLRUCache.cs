05731e318b4ab19436523e7b6e228c942e876105
Igor Iakovlev <iyakovlev@ptsecurity.com>
2017-11-09 14:00:12 +0300

    updated RandomizedLRUCache to lockfree realization

--- sources/LibProtection.Injections/Caching/RandomizedLRUCache.cs
+++ sources/LibProtection.Injections/Caching/RandomizedLRUCache.cs
@@ -1,83 +1,70 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace LibProtection.Injections.Caching
+internal class RandomizedLRUCache<TKey, TValue>
 {
-    internal class RandomizedLRUCache<TKey, TValue>
+    private class CacheRecord
     {
-        private class CacheRecord
+        public TKey Key;
+        public TValue Value;
+        public int Counter;
+    }
+
+    private static readonly Random seedSource = new Random(DateTime.Now.Millisecond);
+
+    [ThreadStatic]
+    private static Random _random;
+
+    private readonly ConcurrentDictionary<TKey, CacheRecord> _cache;
+    private readonly CacheRecord[] _records;
+    private readonly Func<TKey, TValue> _valueFactory;
+    private readonly Func<TKey, CacheRecord> _recordFactory;
+
+    public RandomizedLRUCache(Func<TKey, TValue> factory, int capacity, IEqualityComparer<TKey> comparer = null)
+    {
+        _cache = new ConcurrentDictionary<TKey, CacheRecord>(comparer ?? EqualityComparer<TKey>.Default);
+        _records = new CacheRecord[capacity];
+        _valueFactory = factory;
+        _recordFactory = RecordFactory;
+    }
+
+    public TValue Get(TKey key)
+    {
+        var record = _cache.GetOrAdd(key, _recordFactory);
+        var index = GetRandomIndex();
+        var oldRecord = Interlocked.Exchange(ref _records[index], record);
+
+        if (oldRecord == null)
         {
-            public TKey Key;
-            public TValue Value;
-            public int Counter;
+            Interlocked.Increment(ref record.Counter);
         }
-
-        private readonly ReaderWriterLockSlim _locker = new ReaderWriterLockSlim();
-        private readonly Dictionary<TKey, CacheRecord> _cache;
-        private readonly CacheRecord[] _records;
-        private readonly Random _random;
-
-        public RandomizedLRUCache(int capacity, IEqualityComparer<TKey> comparer = null)
+        else if (oldRecord != record)
         {
-            _cache = new Dictionary<TKey, CacheRecord>(capacity + 1, comparer ?? EqualityComparer<TKey>.Default);
-            _records = new CacheRecord[capacity];
-            _random = new Random(DateTime.Now.Millisecond);
-        }
-
-        public TValue Get(TKey key, Func<TKey, TValue> factory)
-        {
-            try
+            Interlocked.Increment(ref record.Counter);
+            if (Interlocked.Decrement(ref oldRecord.Counter) == 0)
             {
-                _locker.EnterUpgradeableReadLock();
-
-                CacheRecord record;
-                if (!_cache.TryGetValue(key, out record))
-                {
-                    var value = factory(key);
-
-                    _locker.EnterWriteLock();
-
-                    if (!_cache.TryGetValue(key, out record))
-                    {
-                        record = new CacheRecord
-                        {
-                            Key = key,
-                            Value = value,
-                        };
-                        _cache.Add(key, record);
-                    }
-                }
-                else
-                {
-                    _locker.EnterWriteLock();
-                }
-
-                var index = _random.Next(_records.Length);
-                var oldRecord = _records[index];
-                _records[index] = record;
-
-                if (oldRecord == null)
-                {
-                    ++record.Counter;
-                }
-                else if (oldRecord != record)
-                {
-                    ++record.Counter;
-
-                    if (--oldRecord.Counter == 0)
-                    {
-                        _cache.Remove(oldRecord.Key);
-                    }
-                }
-
-                return record.Value;
-            }
-            finally
-            {
-                if (_locker.IsWriteLockHeld) { _locker.ExitWriteLock(); }
-                if (_locker.IsUpgradeableReadLockHeld) { _locker.ExitUpgradeableReadLock(); }
+                _cache.TryRemove(oldRecord.Key, out oldRecord);
             }
         }
+
+        return record.Value;
+    }
+
+    private CacheRecord RecordFactory(TKey key) =>
+        new CacheRecord { Key = key, Value = _valueFactory(key) };
+
+    private int GetRandomIndex()
+    {
+        if (_random == null)
+        {
+            lock (seedSource)
+            {
+                _random = new Random(seedSource.Next());
+            }
+        }
+
+        return _random.Next(_records.Length);
     }
 }
