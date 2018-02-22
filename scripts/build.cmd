c09b511dcbe04eb52b8a4cd6d2f077717611639b
Vladimir Kochetkov <kochetkov.vladimir@gmail.com>
2018-02-22 16:11:50 +0300

    Yet another CI build fixes

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,14 +1,18 @@
+dotnet restore ..\sources\LibProtection.Injections
+dotnet build -c Release ..\sources\LibProtection.Injections
+
+dotnet restore ..\sources\LibProtection.Injections.Tests
+dotnet build -c Release ..\sources\LibProtection.Injections.Tests
+
 ..\tools\nuget\nuget.exe restore ..\sources\LibProtection.sln
-dotnet build ..\sources\LibProtection.Injections\LibProtection.Injections.csproj
-dotnet build ..\sources\LibProtection.Injections.Tests\LibProtection.Injections.Tests.csproj
 
 set vs2017dir=%programfiles(x86)%\Microsoft Visual Studio\2017
 set msbuild="%vs2017dir%\Community\MSBuild\15.0\Bin\MSBuild.exe"
 if not exist %msbuild% (
-    set msbuild="%vs2017dir%\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+   set msbuild="%vs2017dir%\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 )
 if not exist %msbuild% (
-    set msbuild="%vs2017dir%\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+     set msbuild="%vs2017dir%\Professional\MSBuild\15.0\Bin\MSBuild.exe"
 )
 
-%msbuild% ..\sources\LibProtection.TestSite\LibProtection.TestSite.csproj /t:Rebuild /p:Configuration=Release /p:Platform=AnyCPU
+%msbuild% ..\sources\LibProtection.TestSite\LibProtection.TestSite.csproj /t:Build /p:Configuration=Release /p:Platform=AnyCPU
