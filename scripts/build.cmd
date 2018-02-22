f21c026a3c7b2de243cd7ce94a72d0252a40e76e
Vladimir Kochetkov <kochetkov.vladimir@gmail.com>
2018-02-22 14:30:04 +0300

    Fixed CI's build process

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,1 +1,14 @@
-dotnet build ..\sources\LibProtection.sln
+..\tools\nuget\nuget.exe restore ..\sources\LibProtection.sln
+dotnet build ..\sources\LibProtection.Injections\LibProtection.Injections.csproj
+dotnet build ..\sources\LibProtection.Injections.Tests\LibProtection.Injections.Tests.csproj
+
+set vs2017dir=%programfiles(x86)%\Microsoft Visual Studio\2017
+set msbuild="%vs2017dir%\Community\MSBuild\15.0\Bin\MSBuild.exe"
+if not exist %msbuild% (
+    set msbuild="%vs2017dir%\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+)
+if not exist %msbuild% (
+    set msbuild="%vs2017dir%\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+)
+
+%msbuild% ..\sources\LibProtection.TestSite\LibProtection.TestSite.csproj /t:Rebuild /p:Configuration=Release
