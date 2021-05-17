5014aa058073fad24b22a34bab47840488539758
okhabarov <khabar.oleg@gmail.com>
2021-05-17 10:01:22 +0300

    Fixed incorrect formatting of interpolated strings

---
+++ sources/LibProtection.Injections/LibProtection.Injections/RawString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LibProtection.Injections
+{
+    public class RawString
+    {
+        public string Value { get; }
+
+        private RawString(string str)
+        {
+            Value = str;
+        }
+
+        public static implicit operator RawString(string str)
+        {
+            return new RawString(str);
+        }
+
+        public static implicit operator RawString(FormattableString formattable)
+        {
+            return new RawString(formattable.ToString());
+        }
+    }
+}
